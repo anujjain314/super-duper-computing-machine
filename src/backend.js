6ceb6c7bd00171db63efc89d60960abf08d40e07
John Doe <anujjain314@utexas.edu>
2022-10-24 01:35:18 -0500

    fixed url

--- src/backend.js
+++ src/backend.js
@@ -1,6 +1,6 @@
 async function callBackend(endpoint) {
   console.log("calling backend");
-  const url = new URL(`http://127.0.0.1/${endpoint}`);
+  const url = new URL(`https://anujjain314-projects-app.herokuapp.com/${endpoint}`);
   const response = await fetch(url.toString());
   const data = await response.json();
   alert(data.value);
